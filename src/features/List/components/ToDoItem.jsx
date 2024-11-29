d32be704c7dba744332a42c42a3bdbcf4ad76275
nikolaborenovic <n97.borenovic@gmail.com>
2024-11-29 18:33:29 +0100

    Finishing button functions

--- src/features/List/components/ToDoItem.jsx
+++ src/features/List/components/ToDoItem.jsx
@@ -1,9 +1,17 @@
 import DeleteImg from '../../../assets/delete.png'
 import EditImg from '../../../assets/edit.png'
 import '../list.style.css'
+import { useToDoStore } from "../../../store/todo/todo.store";
 
 const ToDoItem = ({task}) => {
-    console.log(task, 'jedan item')
+    const {deleteTask, todoIsCompleted} = useToDoStore();
+    const handleToDoDelete = () => {
+        deleteTask(task.id)
+    }
+    const handleCompletedToDo = () => {
+        todoIsCompleted(task.id)
+    }
+
     return(
         <div className='item_holder'>
             <div className='item_text'>
@@ -18,14 +26,22 @@ const ToDoItem = ({task}) => {
             height={30}
             stroke="#22C55E"
             fill={task.isCompleted ? '#22C55E' : '#ffffff'}
+            onClick={() => handleCompletedToDo()}
             >
                 <circle cx="11.998" cy="11.998" fillRule="nonzero" r="9.998" />
                 </svg>
-                <p>{task.title}</p>
+                <p
+                // style={task.isCompleted ? {textDecoration:'line-through'}: {}}
+                style={{
+                    textDecoration: task.isCompleted ? 'line-through' : 'none'
+                }}
+                >{task.title}</p>
             </div>
             <div className='task_img'>
                 <img src={EditImg}/>
-                <img src={DeleteImg}/>
+                <img src={DeleteImg}
+                onClick={() => handleToDoDelete()}
+                />
                 </div>
         </div>
     )
--- src/store/todo/todo.store.js
+++ src/store/todo/todo.store.js
@@ -5,5 +5,19 @@ export const useToDoStore = create((set, get) => ({
     addTask: (newTask) => {
         const allToDo = get().todoList;
         set({todoList: [...allToDo, newTask]})
+    },
+
+    deleteTask: (id) => {
+        const allToDo = get().todoList;
+        const allToDoAfterDelete = allToDo.filter((todo) => todo.id !== id);
+        set({ todoList: allToDoAfterDelete })
+    },
+
+    todoIsCompleted: (idMarkedCompleted) => {
+        const allToDo = get().todoList;
+        const markedToDoCompleted = allToDo.map((todo) =>
+            todo.id === idMarkedCompleted ? {...todo, isCompleted: !todo.isCompleted} : todo
+        );
+        set({ todoList: markedToDoCompleted })
     }
 }))
